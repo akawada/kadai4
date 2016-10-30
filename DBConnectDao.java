58e79db09d9e8fade0b70476bbed16b045876a64
akawada <atsushicco51@hotmail.com>
2016-10-30 22:49:04 +0900

    kadai4-2

--- DBConnectDao.java
+++ DBConnectDao.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DBConnectDao {
-	public static <BookList> List<BookList> conectOracle() throws Exception{
+	public <T> List<T> conectOracle() throws Exception{
 		/* ユーザ名 */
 		String user = "system";
 		/* パスワード */
